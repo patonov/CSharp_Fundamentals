39044c9a2cda395bb69ac9511663a54788f07bde
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-06 15:01:28 +0200

    Add files via upload

---
+++ Stdent Information.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string name = Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+        double grade = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Name: {0}, Age: {1}, Grade: {2:F2}", name, age, grade);
+
+    }
+}
