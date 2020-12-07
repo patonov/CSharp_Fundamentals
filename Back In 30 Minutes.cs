46a12b07d3a9caced0babb56ab324ecc4288d23d
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-07 20:51:32 +0200

    Add files via upload

---
+++ Back In 30 Minutes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Back_In_30_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            var time = new TimeSpan(hours, minutes + 30, 0);
+            Console.WriteLine("{0:h\\:mm}", time);
+
+        }
+    }
+}
