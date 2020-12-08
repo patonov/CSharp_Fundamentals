562bf5c159df65184ba73bceb7a118b6341f3508
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-08 17:01:47 +0200

    Add files via upload

---
+++ Sum of Odd Numbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_Odd_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int counter = 0;
+
+            for (int i = 1; i <= 100; i += 2)
+            {
+                Console.WriteLine(i);
+                counter++;
+                sum += i;
+
+                if (counter == n)
+                {
+                    Console.WriteLine("Sum: {0}", sum);
+                    break;
+                }
+            }
+        }
+    }
+}
