fd21719d5cec75e806219fb8b88bc941386f4120
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-08 17:08:14 +0200

    Add files via upload

---
+++ Multiplication Table 2.0.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplication_Table_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int nStart = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            if (nStart > 10)
+            {
+                sum = n * nStart;
+                Console.WriteLine("{0} X {1} = {2}", n, nStart, sum);
+            }
+
+            for (int i = nStart; i <= 10; i++)
+            {
+                sum = n * i;
+                Console.WriteLine("{0} X {1} = {2}", n, i, sum);
+            }
+        }
+    }
+}
