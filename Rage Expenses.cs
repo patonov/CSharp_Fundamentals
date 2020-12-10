41bfc0e7a27600cd1cbe8cc6a1d3d7cd213f5d04
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-10 13:33:24 +0200

    Add files via upload

---
+++ Rage Expenses.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rage_Expenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGames = int.Parse(Console.ReadLine());
+            double headSetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+
+            double money = headSetPrice * (lostGames / 2);
+            money += mousePrice * (lostGames / 3);
+            money += keyboardPrice * (lostGames / 6);
+            money += displayPrice * (lostGames / 12);
+
+            Console.WriteLine("Rage expenses: {0:F2} lv.", money);
+        }
+    }
+}
