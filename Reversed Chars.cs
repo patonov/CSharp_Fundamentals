fcf53a067554dbc609e6e6acba7bfe3bbee3cbcd
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-11 17:03:45 +0200

    Add files via upload

---
+++ Reversed Chars.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reversed_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char nOne = char.Parse(Console.ReadLine());
+            char nTwo = char.Parse(Console.ReadLine());
+            char nThree = char.Parse(Console.ReadLine());
+
+            Console.Write("{0} ", nThree);
+            Console.Write("{0} ", nTwo);
+            Console.Write("{0} ", nOne);
+
+        }
+    }
+}
