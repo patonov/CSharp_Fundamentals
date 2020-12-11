aae600fecca08ad8d44dccbbae8d46ae55a0f9ac
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-11 17:05:51 +0200

    Add files via upload

---
+++ Town Info.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Town_Info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string town = Console.ReadLine();
+            int population = int.Parse(Console.ReadLine());
+            int area = int.Parse(Console.ReadLine());
+
+
+            Console.Write("Town {0} has population of {1} and area {2} square km.", town, population, area);
+        }
+    }
+}
