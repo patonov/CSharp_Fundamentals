054a4be303e8098daffb6071dce406f3bd4b3aac
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-11 17:03:16 +0200

    Add files via upload

---
+++ Special Numbers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                int sum = 0;
+                int num = i;
+
+                for (int e = 0; e < i.ToString().Length; e++)
+                {
+                    sum += num % 10;
+                    num /= 10;
+                }
+
+                if (sum == 5 || sum == 7 || sum == 11)
+                {
+                    Console.WriteLine("{0} -> True", i);
+                }
+                else
+                {
+                    Console.WriteLine("{0} -> False", i);
+                }
+            }
+        }
+    }
+}
