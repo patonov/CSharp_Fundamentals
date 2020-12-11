6ca717014d0688166bae241ff29ba457c33fe2ef
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-11 17:06:35 +0200

    Add files via upload

---
+++ Chars to String.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chars_to_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char nOne = char.Parse(Console.ReadLine());
+            char nTwo = char.Parse(Console.ReadLine());
+            char nThree = char.Parse(Console.ReadLine());
+
+
+            Console.Write("{0}{1}{2}", nOne, nTwo, nThree);
+        }
+    }
+}
