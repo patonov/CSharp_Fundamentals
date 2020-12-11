882371274c6c1f92e018b2c90bdc3001ce77a3e6
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-11 17:01:39 +0200

    Add files via upload

---
+++ Pounds to Dollars.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pounds_to_Dollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal pounds = decimal.Parse(Console.ReadLine());
+
+
+            decimal dollars = pounds * 1.31M;
+
+
+            Console.WriteLine("{0:F3}", dollars);
+        }
+    }
+}
