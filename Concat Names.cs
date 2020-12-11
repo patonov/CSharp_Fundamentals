0cffb6590c6de8556c6d26119a6169979c4da551
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-11 17:04:11 +0200

    Add files via upload

---
+++ Concat Names.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concat_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameOne = Console.ReadLine();
+            string nameTwo = Console.ReadLine();
+            string delimiter = Console.ReadLine();
+
+
+
+            Console.Write("{0}", nameOne);
+            Console.Write("{0}", delimiter);
+            Console.Write("{0}", nameTwo);
+
+        }
+    }
+}
