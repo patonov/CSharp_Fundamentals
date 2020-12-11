7ee5a278387d81270a18321ce988630264edc632
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-11 17:01:09 +0200

    Add files via upload

---
+++ Convert Meters to Kilometers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert_Meters_to_Kilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            decimal nKm = (decimal)n / 1000;
+
+
+            Console.WriteLine("{0:F2}", nKm);
+
+        }
+    }
+}
