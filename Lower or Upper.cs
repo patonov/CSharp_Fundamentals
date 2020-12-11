c3f7e2840138545ca92a4a1ad3b822ba48816348
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-11 17:07:22 +0200

    Add files via upload

---
+++ Lower or Upper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lower_or_Upper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter = char.Parse(Console.ReadLine());
+
+            if (letter >= 65 && letter <= 90)
+            {
+                Console.Write("upper-case");
+            }
+            else if (letter >= 97 && letter <= 122)
+            {
+                Console.Write("lower-case");
+            }
+        }
+    }
+}
