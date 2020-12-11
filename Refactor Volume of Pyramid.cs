65f6a41bb206a7666d680643967097d8ad7ffd3b
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-11 17:07:53 +0200

    Add files via upload

---
+++ Refactor Volume of Pyramid.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refactor_Volume_of_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double heights = double.Parse(Console.ReadLine());
+
+            double perimeter = length * width;
+            double volume = (perimeter * heights) / 3;
+
+            Console.Write("Length: ");
+            Console.Write("Width: ");
+            Console.Write("Height: ");
+            Console.WriteLine("Pyramid Volume: {0:f2}", volume);
+
+        }
+    }
+}
