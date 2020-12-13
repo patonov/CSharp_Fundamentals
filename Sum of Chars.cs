13ef3f0dbca99f84f0a496fa4193ba857c223e42
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-13 20:21:35 +0200

    Add files via upload

---
+++ Sum of Chars.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                char input = char.Parse(Console.ReadLine());
+                sum += input;
+            }
+            Console.WriteLine("The sum equals: {0}", sum);
+        }
+    }
+}
