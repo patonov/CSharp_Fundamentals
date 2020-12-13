e0c4f3dec74dd2933a4117dcae170728ef90956c
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-13 20:22:22 +0200

    Add files via upload

---
+++ Print Part Of ASCII Table.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print_Part_Of_ASCII_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startChart = int.Parse(Console.ReadLine());
+            int endChart = int.Parse(Console.ReadLine());
+
+            for (int i = startChart; i <= endChart; i++)
+            {
+                Console.Write("{0} ", (char)i);
+            }
+
+        }
+    }
+}
