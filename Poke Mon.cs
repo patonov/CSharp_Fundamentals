21ca8aa879bf784c88a0b9d7393f4cc001f2844c
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-13 20:25:59 +0200

    Add files via upload

---
+++ Poke Mon.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poke_Mon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int power = int.Parse(Console.ReadLine());
+            int distance = int.Parse(Console.ReadLine());
+            int exaustionFactor = int.Parse(Console.ReadLine());
+
+            double tempPower = power * 0.5;
+            int pokeCount = 0;
+
+            while (power >= distance)
+            {
+                pokeCount++;
+                power -= distance;
+
+                if (power == tempPower && exaustionFactor != 0)
+                {
+                    power /= exaustionFactor;
+                }
+            }
+            Console.WriteLine(power);
+            Console.WriteLine(pokeCount);
+        }
+    }
+}
