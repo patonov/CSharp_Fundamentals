7eb887fbfafae8abacbd6c1542e3b46609723553
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-13 20:23:21 +0200

    Add files via upload

---
+++ Triples of Latin Letters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triples_of_Latin_Letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int firstSybol = 0; firstSybol < n; firstSybol++)
+            {
+                for (int secondSybol = 0; secondSybol < n; secondSybol++)
+                {
+                    for (int thirdSybol = 0; thirdSybol < n; thirdSybol++)
+                    {
+                        char firstChar = (char)(97 + firstSybol);
+                        char secondChar = (char)(97 + secondSybol);
+                        char thirdChar = (char)(97 + thirdSybol);
+
+                        Console.WriteLine("{0}{1}{2}", firstChar, secondChar, thirdChar);
+                    }
+
+                }
+
+            }
+        }
+    }
+}
