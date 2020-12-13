1a5cf0af29b78ce9b5ce53f91ad262373b0e2c1e
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-13 20:19:59 +0200

    Add files via upload

---
+++ Integer Operations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integer_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberFirst = int.Parse(Console.ReadLine());
+            int numberSecond = int.Parse(Console.ReadLine());
+            int numberThird = int.Parse(Console.ReadLine());
+            int numberFourth = int.Parse(Console.ReadLine());
+
+            int sum = (numberFirst + numberSecond) / numberThird * numberFourth;
+            Console.WriteLine(sum);
+        }
+    }
+}
