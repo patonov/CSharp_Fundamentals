1063b79f35d7687062d33521db015b81e650411e
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-14 19:24:37 +0200

    Add files via upload

---
+++ Condense Array to Number.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condense_Array_to_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] condensed = new int[numbers.Length - 1];
+
+            if (numbers.Length == 1)
+            {
+                Console.WriteLine(numbers[0]);
+                return;
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < condensed.Length - i; j++)
+                {
+                    condensed[j] = numbers[j] + numbers[j + 1];
+                }
+                numbers = condensed;
+            }
+            Console.WriteLine(condensed[0]);
+
+        }
+    }
+}
