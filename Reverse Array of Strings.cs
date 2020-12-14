3f76a2d023a665c7cf3ca60481fd50572aab82fd
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-14 19:22:53 +0200

    Add files via upload

---
+++ Reverse Array of Strings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Array_of_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ').ToArray();
+
+
+
+            Console.Write(string.Join(" ", words.Reverse()));
+
+        }
+    }
+}
