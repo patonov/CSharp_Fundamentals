df8eff98597b9a5b38e47ccc8cd93ad4b5be4cba
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-14 19:21:12 +0200

    Add files via upload

---
+++ Day of Week.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_of_Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days = new String[]
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            int dayNum = int.Parse(Console.ReadLine());
+
+            if (dayNum < 1 || dayNum > days.Length)
+            {
+                Console.WriteLine("Invalid day!");
+            }
+            else
+            {
+                Console.WriteLine(days[dayNum - 1]);
+            }
+
+        }
+    }
+}
