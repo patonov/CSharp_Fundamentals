d5ff614b13b7987450d4860f9a4d6837a52c6040
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-14 19:23:19 +0200

    Add files via upload

---
+++ Sum Even Numbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int digit = numbers[i];
+
+                if (digit % 2 == 0)
+                {
+                    sum += digit;
+                }
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
