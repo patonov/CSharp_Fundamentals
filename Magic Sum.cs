736241a8f6b06a41a0e5dfc28397bcd4fa95bf54
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-15 13:27:19 +0200

    Add files via upload

---
+++ Magic Sum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magic_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int number = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int firstNumber = array[i];
+
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    int secondNumber = array[j];
+
+                    if (firstNumber + secondNumber == number)
+                    {
+                        Console.WriteLine("{0} {1}", firstNumber, secondNumber);
+                        break;
+                    }
+                }
+
+            }
+        }
+    }
+}
