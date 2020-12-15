132f1f58090588e2635109b10b6f4ed291ceb253
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-15 13:31:18 +0200

    Add files via upload

---
+++ Array Rotation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Rotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Console.ReadLine().Split().ToArray();
+            int n = int.Parse(Console.ReadLine());
+            for (int j = 0; j < n; j++)
+            {
+                var end = arr[0];
+                for (var i = 0; i < arr.Length - 1; i++)
+                {
+                    arr[i] = arr[i + 1];
+                }
+                arr[arr.Length - 1] = end;
+            }
+            Console.WriteLine(string.Join(" ", arr));
+
+        }
+    }
+}
