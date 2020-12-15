617ecacb68cd1d790a647514051ba7e80c327c91
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-15 13:30:53 +0200

    Add files via upload

---
+++ Top Integers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Top_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            bool isBigger = true;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int currentElement = array[i];
+
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] >= currentElement)
+                    {
+                        isBigger = false;
+                        break;
+                    }
+                }
+
+                if (isBigger)
+                {
+                    Console.Write("{0} ", currentElement);
+                }
+
+                isBigger = true;
+            }
+        }
+    }
+}
