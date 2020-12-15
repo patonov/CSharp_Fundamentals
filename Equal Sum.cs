33bda640ad994ddb5071bbe09150ada4238ba1fd
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-15 13:28:18 +0200

    Add files via upload

---
+++ Equal Sum.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equal_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            bool isFound = false;
+
+            for (int curr = 0; curr < array.Length; curr++)
+            {
+                int sumRight = 0;
+
+                for (int i = curr + 1; i < array.Length; i++)
+                {
+                    sumRight += array[i];
+                }
+
+                int sumLeft = 0;
+                for (int i = curr - 1; i >= 0; i--)
+                {
+                    sumLeft += array[i];
+                }
+
+                if (sumRight == sumLeft)
+                {
+                    Console.WriteLine(curr);
+                    isFound = true;
+                    break;
+                }
+            }
+            if (!isFound)
+            {
+                Console.WriteLine("no");
+            }
+
+
+        }
+    }
+}
