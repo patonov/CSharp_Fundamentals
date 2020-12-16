c9971f4430377549d93789f1ca744aca7226fd59
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-16 23:58:06 +0200

    Add files via upload

---
+++ Sign of Integer Numbers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sign_of_Integer_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+
+            PrintSignNumber(number);
+
+
+        }
+
+        static void PrintSignNumber(int number)
+        {
+            if (number < 0)
+            {
+                Console.WriteLine("The number {0} is negative.", number);
+            }
+            else if (number > 0)
+            {
+                Console.WriteLine("The number {0} is positive.", number);
+            }
+            else
+            {
+                Console.WriteLine("The number {0} is zero.", number);
+            }
+
+        }
+    }
+    }
+
