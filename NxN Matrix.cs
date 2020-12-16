b1c7fac4597e5e86cc435a4b293e1fb36b1da7b1
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-17 00:21:12 +0200

    Add files via upload

---
+++ NxN Matrix.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NxN_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int digit = int.Parse(Console.ReadLine());
+            int matrix = PrintMatrix(digit);
+
+
+        }
+
+        static int PrintMatrix(int num)
+        {
+            for (int j = 0; j < num; j++)
+            {
+                for (int i = 0; i < num; i++)
+                {
+                    Console.Write(num + " ");
+                }
+                Console.WriteLine();
+            }
+            return num;
+        }
+    }
+    }
+
