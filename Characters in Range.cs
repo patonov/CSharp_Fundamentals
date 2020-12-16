fb17fd9e455f42b233a0036f7f7f1d06115df732
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-17 00:19:14 +0200

    Add files via upload

---
+++ Characters in Range.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Characters_in_Range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+
+            PrintInBetween(firstChar, secondChar);
+
+        }
+
+
+        public static void PrintInBetween(char firstChar, char secondChar)
+        {
+            if (firstChar > secondChar)
+            {
+                char first = firstChar;
+                firstChar = secondChar;
+                secondChar = first;
+            }
+            for (int i = firstChar + 1; i < secondChar; i++)
+            {
+                Console.Write((char)i + " ");
+            }
+        }
+    }
+}
