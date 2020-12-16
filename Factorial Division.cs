b032e9499f1e3d88d3754814d01edcc7f14fde46
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-17 00:21:52 +0200

    Add files via upload

---
+++ Factorial Division.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial_Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int digitOne = int.Parse(Console.ReadLine());
+            int digitTwo = int.Parse(Console.ReadLine());
+
+            double calculateFactorialFirstDigit = GetFactorial(digitOne);
+            double calculateFactorialSecondDigit = GetFactorial(digitTwo);
+
+            double result = calculateFactorialFirstDigit / calculateFactorialSecondDigit;
+
+            Console.WriteLine("{0:F2}", result);
+        }
+        static double GetFactorial(double num)
+        {
+            double result = 1;
+            while (num != 1)
+            {
+                result = result * num;
+                num = num - 1;
+            }
+            return result;
+        }
+    }
+    }
+
