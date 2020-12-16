25b03a7e81371330dd04a51ef3d1b33c41d4b12c
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-16 23:58:54 +0200

    Add files via upload

---
+++ Vowels Count.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vowels_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine().ToLower();
+
+            int result = PrintVowels(word);
+
+            Console.WriteLine(result);
+        }
+
+        static int PrintVowels(string word)
+        {
+            int counter = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                char currentChar = word[i];
+
+                if (currentChar == 'a')
+                {
+                    counter++;
+                }
+                else if (currentChar == 'e')
+                {
+                    counter++;
+                }
+                else if (currentChar == 'i')
+                {
+                    counter++;
+                }
+                else if (currentChar == 'o')
+                {
+                    counter++;
+                }
+                else if (currentChar == 'u')
+                {
+                    counter++;
+                }
+                else if (currentChar == 'y')
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+    }
+    }
+
