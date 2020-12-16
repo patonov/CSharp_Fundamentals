ca178bbea1424b1a933ea9d545a5add5fb9e9905
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-17 00:20:08 +0200

    Add files via upload

---
+++ Add and Subtract.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Add_and_Subtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOne = int.Parse(Console.ReadLine());
+            int numTwo = int.Parse(Console.ReadLine());
+            int numThree = int.Parse(Console.ReadLine());
+
+            int sum = SumOfFirstTwo(numOne, numTwo);
+            int diff = DifferenceOfThird(numOne, numTwo, numThree);
+
+            Console.WriteLine(diff);
+        }
+
+        static int SumOfFirstTwo(int numOne, int numTwo)
+        {
+            int sum = numOne + numTwo;
+            return sum;
+        }
+
+        static int DifferenceOfThird(int numOne, int numTwo, int numThree)
+        {
+            int sum = numOne + numTwo;
+            int difference = sum - numThree;
+            return difference;
+        }
+    }
+    }
+}
