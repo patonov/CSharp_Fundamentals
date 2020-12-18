1ce18bd557029104dec5e07e10c33120cb6ec229
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-18 21:06:36 +0200

    Add files via upload

---
+++ Cards Game.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cards_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstPlayer = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> secondPlayer = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            while (firstPlayer.Count != 0 && secondPlayer.Count != 0)
+            {
+                int firstPlayerCard = firstPlayer[0];
+                int secondPlayerCard = secondPlayer[0];
+                if (firstPlayerCard > secondPlayerCard)
+                {
+                    firstPlayer.Add(firstPlayerCard);
+                    firstPlayer.Add(secondPlayerCard);
+                }
+                else if (firstPlayerCard < secondPlayerCard)
+                {
+                    secondPlayer.Add(secondPlayerCard);
+                    secondPlayer.Add(firstPlayerCard);
+                }
+
+                firstPlayer.RemoveAt(0);
+                secondPlayer.RemoveAt(0);
+
+            }
+
+            if (firstPlayer.Count == 0)
+            {
+                Console.WriteLine("Second player wins! Sum: {0}", secondPlayer.Sum());
+            }
+            else
+            {
+                Console.WriteLine("First player wins! Sum: {0}", firstPlayer.Sum());
+            }
+
+
+        }
+    }
+}
