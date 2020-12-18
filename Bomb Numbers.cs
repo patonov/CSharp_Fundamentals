b327f998d8ed3027008802759e6079260a32cf83
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-18 21:06:03 +0200

    Add files via upload

---
+++ Bomb Numbers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bomb_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            int[] bombProps = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int bomb = bombProps[0];
+            int bombPower = bombProps[1];
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int currentNumber = numbers[i];
+                if (currentNumber == bomb)
+                {
+                    int startIndex = i - bombPower;
+                    int endIndex = i + bombPower;
+
+                    if (startIndex < 0)
+                    {
+                        startIndex = 0;
+                    }
+                    if (endIndex > numbers.Count - 1)
+                    {
+                        startIndex = numbers.Count - 1;
+                    }
+                    int endIndexToRemove = endIndex - startIndex + 1;
+                    numbers.RemoveRange(startIndex, endIndexToRemove);
+
+                    i = startIndex - 1;
+                }
+
+            }
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
