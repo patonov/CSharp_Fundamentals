5a2b521ac9aed291aca8bef6dd7c9895edfdd3f9
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-18 21:07:03 +0200

    Add files via upload

---
+++ Append Arrays.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Append_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> items = Console.ReadLine().Split('|').ToList();
+
+            items.Reverse();
+
+            List<string> result = new List<string>();
+
+            foreach (string item in items)
+            {
+                string[] numbers = item.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                foreach (string numbersToAdd in numbers)
+                {
+                    result.Add(numbersToAdd);
+                }
+            }
+            Console.WriteLine(string.Join(" ", result));
+
+        }
+    }
+}
