4c70242de3d8ce79e2faf39377153763f865d9ed
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-18 21:04:44 +0200

    Add files via upload

---
+++ Change List.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Change_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listOfInts = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                string[] commandArg = command.Split();
+                string firstArgument = commandArg[0];
+                int element = int.Parse(commandArg[1]);
+
+                if (firstArgument == "Delete")
+                {
+                    listOfInts.RemoveAll(x => x == element);
+                }
+                else if (firstArgument == "Insert")
+                {
+                    int position = int.Parse(commandArg[2]);
+                    listOfInts.Insert(position, element);
+                }
+
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ", listOfInts));
+
+        }
+    }
+}
