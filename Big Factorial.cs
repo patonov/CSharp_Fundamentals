bfb6cc8a58a723a9ddddf7a715616244f47ff580
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-19 15:35:40 +0200

    Add files via upload

---
+++ Big Factorial.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Big_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        int number = int.Parse(Console.ReadLine());
+        BigInteger result = 1;
+		
+		for (int i = 1; i <= number; i++)
+		{
+		result *= i;
+		}
+    Console.WriteLine(result);
+    }
+}
