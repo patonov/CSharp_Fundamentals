e460ee6f4b77b241b18ba643779f34a3157332ef
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-19 15:35:18 +0200

    Add files via upload

---
+++ Randomize Words.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Randomize_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                int index = rnd.Next(0, words.Length);
+                string word = words[i];
+
+                words[i] = words[index];
+                words[index] = word;
+            }
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
