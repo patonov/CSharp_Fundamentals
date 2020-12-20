97abeece82bba31f171a05c2d2d1460ffd3fde5f
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-20 20:00:43 +0200

    Add files via upload

---
+++ Largest 3 Numbers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Largest_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            SortedDictionary<int, int> numbers = new SortedDictionary<int, int>();
+            int counter = 0;
+
+            foreach (var item in input)
+            {
+                if (!numbers.ContainsKey(item))
+                {
+                    numbers.Add(item, 0);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            foreach (var item in numbers.OrderByDescending(n => n.Key))
+            {
+                counter++;
+                Console.Write(item.Key + " ");
+                if (counter >= 3)
+                {
+                    break;
+                }
+            }
+
+        }
+    }
+}
