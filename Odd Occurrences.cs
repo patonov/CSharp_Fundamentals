48c5668b6f4728b70bb931225303dae0b1a45b73
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-20 19:59:52 +0200

    Add files via upload

---
+++ Odd Occurrences.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odd_Occurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] things = Console.ReadLine().Split();
+
+            Dictionary<string, int> thingsDict = new Dictionary<string, int>();
+
+            foreach (var thing in things)
+            {
+                string wordLowerCase = thing.ToLower();
+                if (thingsDict.ContainsKey(wordLowerCase))
+                {
+                    thingsDict[wordLowerCase]++;
+                }
+                else
+                {
+                    thingsDict.Add(wordLowerCase, 1);
+                }
+            }
+
+            foreach (var thing in thingsDict)
+            {
+                if (thing.Value % 2 != 0)
+                {
+                    Console.Write(thing.Key + " ");
+                }
+            }
+
+        }
+    }
+}
