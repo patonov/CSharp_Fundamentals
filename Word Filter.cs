2ee5df5c5bd9713cd6e811131656f20ac11419c9
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-20 20:01:05 +0200

    Add files via upload

---
+++ Word Filter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Word_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().Where(x => x.Length % 2 == 0).ToArray();
+
+            foreach (var word in input)
+            {
+                Console.WriteLine(word);
+            }
+
+        }
+    }
+}
