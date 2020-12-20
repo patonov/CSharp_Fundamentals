7ae4349e6821f38013043735dc4f18f4f4ddc6dc
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-20 19:59:27 +0200

    Add files via upload

---
+++ Count Real Numbers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            SortedDictionary<int, int> numbersDict = new SortedDictionary<int, int>();
+
+
+            foreach (var number in numbers)
+            {
+                if (numbersDict.ContainsKey(number))
+                {
+                    numbersDict[number]++;
+                }
+                else
+                {
+                    numbersDict.Add(number, 1);
+                }
+            }
+
+            foreach (var number in numbersDict)
+            {
+                Console.WriteLine("{0} -> {1}", number.Key, number.Value);
+            }
+
+        }
+    }
+}
