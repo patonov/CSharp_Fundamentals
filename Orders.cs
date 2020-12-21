4163ee0445739a87ce728fc0d84cb35b7ebb25c7
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-21 19:13:15 +0200

    Add files via upload

---
+++ Orders.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listProduct = new Dictionary<string, double[]>();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "buy")
+                {
+                    break;
+                }
+                string[] inputProduct = input.Split().ToArray();
+                string product = inputProduct[0];
+                double price = double.Parse(inputProduct[1]);
+                double quantity = double.Parse(inputProduct[2]);
+                if (!listProduct.ContainsKey(product))
+                {
+                    listProduct.Add(product, new double[2]);
+                }
+                listProduct[product][0] = price;
+                listProduct[product][1] += quantity;
+            }
+            foreach (var item in listProduct)
+            {
+                Console.WriteLine("{0} -> {1:f2}", item.Key, (item.Value[0] * item.Value[1]));
+            }
+        }
+    }
+}
