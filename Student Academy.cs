8b26e4bd372786749f18500a2b5da20399f854e6
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-21 19:14:32 +0200

    Add files via upload

---
+++ Student Academy.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Academy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            Dictionary<string, List<double>> listOfGrades = new Dictionary<string, List<double>>();
+
+            string studentName = string.Empty;
+            double studentGrade = 0;
+
+
+            for (int i = 0; i < rows; i++)
+            {
+                studentName = Console.ReadLine();
+                studentGrade = double.Parse(Console.ReadLine());
+
+                if (!listOfGrades.ContainsKey(studentName))
+                {
+
+                    listOfGrades.Add(studentName, new List<double>());
+
+                }
+
+                listOfGrades[studentName].Add(studentGrade);
+
+            }
+
+            foreach (var student in listOfGrades.OrderByDescending(x => x.Value.Average()))
+            {
+
+                if (student.Value.Average() >= 4.5)
+                {
+                    Console.WriteLine("{0} -> {1:f2}", student.Key, student.Value.Average());
+                }
+            }
+        }
+    }
+}
