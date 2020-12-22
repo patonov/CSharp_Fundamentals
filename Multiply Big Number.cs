76307480068675a0e08cd535c521cd0db8119fce
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-22 22:12:32 +0200

    Add files via upload

---
+++ Multiply Big Number.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiply_Big_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bilder = new StringBuilder();
+            string input = Console.ReadLine();
+            int simpleDigit = int.Parse(Console.ReadLine());
+            int rest = 0;
+
+            foreach (char cipher in input.Reverse())
+            {
+                int digit = int.Parse(cipher.ToString());
+                int result = digit * simpleDigit + rest;
+
+                int restDigit = result % 10;
+                rest = result / 10;
+
+                bilder.Insert(0, restDigit);
+            }
+
+            if (rest > 0)
+            {
+                bilder.Insert(0, rest);
+            }
+
+            Console.WriteLine(bilder.ToString());
+
+
+        }
+    }
+}
