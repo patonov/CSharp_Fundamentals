14b44908a3a50944763df46db55593454bda1ab4
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-22 22:12:11 +0200

    Add files via upload

---
+++ Caesar Cipher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caesar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            foreach (char cha in input)
+            {
+                char currentChar = (char)(cha + 3);
+                Console.Write(currentChar);
+
+            }
+        }
+    }
+}
