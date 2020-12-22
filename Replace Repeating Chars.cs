878fd6e6587dfa502e76b35c90d510d72585b0b7
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-22 22:13:02 +0200

    Add files via upload

---
+++ Replace Repeating Chars.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Replace_Repeating_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sb = new StringBuilder();
+            var input = Console.ReadLine().ToCharArray();
+
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                if (input[i] != input[i + 1])
+                {
+                    sb.Append(input[i]);
+
+                }
+
+            }
+
+            var symbol = input[input.Length - 1];
+            sb.Append(symbol);
+
+            Console.WriteLine(sb.ToString());
+
+
+
+        }
+    }
+    }
+
