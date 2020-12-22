44600c6eaff85877aefa791097fa5cb311f145d7
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-22 22:11:47 +0200

    Add files via upload

---
+++ Extract File.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extract_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split('\\');
+
+            var file = input[input.Length - 1];
+
+            var fileArr = file.Split('.');
+
+            var fileName = fileArr[0];
+            var fileExtension = fileArr[1];
+
+            Console.WriteLine("File name: {0}", fileName);
+            Console.WriteLine("File extension: {0}", fileExtension);
+
+
+        }
+    }
+}
