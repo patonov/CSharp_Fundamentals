4b4e08848ec44eb231dfaf2a19efeff44e95c657
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-22 22:13:28 +0200

    Add files via upload

---
+++ String Explosion.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Explosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var input = Console.ReadLine();
+            int bomb = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var currentChar = input[i];
+                if (currentChar == '>')
+                {
+                    bomb += int.Parse(input[i + 1].ToString());
+                    continue;
+                }
+
+                if (bomb > 0)
+                {
+                    input = input.Remove(i, 1);
+                    i--;
+                    bomb--;
+                }
+            }
+            Console.WriteLine(input);
+
+
+
+
+
+        }
+    }
+}
