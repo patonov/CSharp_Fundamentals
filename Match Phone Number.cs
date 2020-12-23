55c6d351c13020617496271c0a5388bec3cba716
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-23 13:53:31 +0200

    Add files via upload

---
+++ Match Phone Number.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Match_Phone_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(\+359 2 \d{3} \d{4}\b)|(\+ ?359-2-\d{3}-\d{4}\b)";
+            string phones = Console.ReadLine();
+
+            MatchCollection phonesMatched = Regex.Matches(phones, pattern);
+
+            var matchPhoneArr = phonesMatched.Cast<Match>().Select(x => x.Value.Trim()).ToArray();
+
+            Console.WriteLine(string.Join(", ", matchPhoneArr));
+        }
+    }
+}
