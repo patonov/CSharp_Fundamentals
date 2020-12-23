beedee47f579aab6c4b5ade2a23e2b92aea5d5c7
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-23 13:53:06 +0200

    Add files via upload

---
+++ Match Full Name.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Match_Full_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+";
+            string names = Console.ReadLine();
+
+            MatchCollection interesting = Regex.Matches(names, pattern);
+
+            foreach (Match item in interesting)
+            {
+                Console.Write(item.Value + ' ');
+            }
+        }
+    }
+}
