4256ade3b401a3705d121b212c6eb3ec5017b3a5
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-23 13:55:05 +0200

    Add files via upload

---
+++ SoftUni Bar Income.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace SoftUni_Bar_Income
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"%([A-Z][a-z]+)%[^|%.]*<(\w+)>[^|%.]*\|(\d+)\|[^|%.]*?(\d+\.?\d*)\$";
+
+            Regex regex = new Regex(pattern);
+
+            string input = Console.ReadLine();
+            double totalPrice = 0;
+
+            while (input != "end of shift")
+            {
+                Match match = regex.Match(input);
+
+
+                if (match.Success)
+                {
+                    string customer = match.Groups[1].Value;
+                    string product = match.Groups[2].Value;
+                    int count = int.Parse(match.Groups[3].Value);
+                    double price = double.Parse(match.Groups[4].Value);
+                    double sum = count * price;
+                    totalPrice += sum;
+
+                    Console.WriteLine($"{customer}: {product} - {sum:F2}");
+
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Total income: {totalPrice:F2}");
+
+        }
+    }
+}
