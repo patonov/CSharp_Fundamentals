0fc10593412bb4aa56d172037289e17f912aeab7
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-23 13:54:20 +0200

    Add files via upload

---
+++ Furniture.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @">>([A-Z a-z]+)<<(\d+\.?\d*)!(\d+)";
+
+            Regex regex = new Regex(pattern);
+
+            string input = Console.ReadLine();
+
+            double totalPrice = 0;
+
+            Console.WriteLine("Bought furniture:");
+
+            while (input != "Purchase")
+            {
+                Match match = regex.Match(input);
+
+                if (match.Success)
+                {
+                    string name = match.Groups[1].Value;
+                    double price = double.Parse(match.Groups[2].Value);
+                    int quantity = int.Parse(match.Groups[3].Value);
+                    Console.WriteLine(name);
+
+                    totalPrice += quantity * price;
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine("Total money spend: {0:F2}", totalPrice);
+
+
+        }
+    }
+}
