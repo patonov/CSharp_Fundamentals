c6db208fd9418cd1aad7ecb3ce90b9c22ff2cb86
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-24 17:23:44 +0200

    Add files via upload

---
+++ Heart Delivery.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heart_Delivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] neighborhood = Console.ReadLine().Split('@').Select(int.Parse).ToArray();
+            int jumpPosition = 0;
+            int newJumpPosition = 0;
+
+            string command = Console.ReadLine();
+
+            while (command != "Love!")
+            {
+                string[] comArr = command.Split().ToArray();
+                jumpPosition = int.Parse(comArr[1]);
+                newJumpPosition += jumpPosition;
+
+                if (newJumpPosition > neighborhood.Length - 1)
+                {
+                    newJumpPosition = 0;
+                }
+
+                if (neighborhood[newJumpPosition] == 0)
+                {
+                    Console.WriteLine("Place {0} already had Valentine's day.", newJumpPosition);
+                }
+                else
+                {
+
+                    neighborhood[newJumpPosition] -= 2;
+
+                    if (neighborhood[newJumpPosition] == 0)
+                    {
+                        Console.WriteLine("Place {0} has Valentine's day.", newJumpPosition);
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine("Cupid's last position was {0}.", newJumpPosition);
+
+            int houseCount = 0;
+
+            for (int i = 0; i < neighborhood.Length; i++)
+            {
+                if (neighborhood[i] == 0)
+                {
+                    houseCount++;
+                }
+            }
+
+            if (houseCount == neighborhood.Length)
+            {
+                Console.WriteLine("Mission was successful.");
+            }
+            else
+            {
+                int difference = neighborhood.Length - houseCount;
+                Console.WriteLine("Cupid has failed {0} places.", difference);
+            }
+        }
+    }
+}
